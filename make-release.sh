5bbd97e68ad162efbf6b6e0e62b6ca8f800f64c8
Wayne Piekarski <wayne@tinmith.net>
2018-02-23 19:39:10 -0800

    New archive with XTextureExtractor v0.6, with Java client for Linux, OSX, and Windows

--- make-release.sh
+++ make-release.sh
@@ -2,5 +2,7 @@
 
 cd `dirname $0`
 OUT=./Uploads/XTextureExtractor-NEW.zip
+rm -f $OUT
 set -xv
+rsync -av --exclude="*.class" --exclude=".gitignore" ./java-client/ Plugin-XTextureExtractor-x64-Release/java-client/
 zip -r $OUT Plugin-XTextureExtractor-x64-Release/
