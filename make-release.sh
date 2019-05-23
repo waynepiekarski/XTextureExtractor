8d24949f0272cbeda4a212deaa635bf4de70ece9
Wayne Piekarski <wayne@tinmith.net>
2019-05-22 21:17:52 -0700

    Make the release out of the 2nd plugin directory

--- make-release.sh
+++ make-release.sh
@@ -4,5 +4,5 @@ cd `dirname $0`
 OUT=./Uploads/XTextureExtractor-NEW.zip
 rm -f $OUT
 set -xv
-rsync -av --exclude="*.class" --exclude=".gitignore" ./java-client/ Plugin-XTextureExtractor-x64-Release/java-client/
-zip -r $OUT Plugin-XTextureExtractor-x64-Release/
+rsync -av --exclude="*.class" --exclude=".gitignore" ./java-client/ Plugin2-XTextureExtractor-x64-Release/java-client/
+zip -r $OUT Plugin2-XTextureExtractor-x64-Release/
