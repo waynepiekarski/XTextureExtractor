1ea0b2bc1be3cdf789a35332f55d8a5c616ac3db
Wayne Piekarski <wayne@tinmith.net>
2022-06-12 23:08:14 -0700

    Update release script to include the README file

--- make-release.sh
+++ make-release.sh
@@ -5,4 +5,5 @@ OUT=./Uploads/XTextureExtractor-NEW.zip
 rm -f $OUT
 set -xv
 rsync -av --exclude="*.class" --exclude=".gitignore" ./java-client/ Plugin-XTextureExtractor-x64-Release/java-client/
+cp README Plugin-XTextureExtractor-x64-Release/
 zip -r $OUT Plugin-XTextureExtractor-x64-Release/
