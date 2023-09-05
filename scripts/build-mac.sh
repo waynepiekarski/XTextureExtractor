726a15a677448212b3f93acb802b66cdffcf3017
Wayne Piekarski <wayne@tinmith.net>
2023-09-05 15:43:27 -0700

    Added -arch x86_64 to make compilation work with the existing SDK on newer arm64 build machines

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -3,4 +3,10 @@
 # https://developer.x-plane.com/article/building-and-installing-plugins/
 cd `dirname $0`/..
 set -x
-g++ -flat_namespace -undefined suppress -std=c++17 -fPIC -DXPLM301 -DXPLM300 -DXPLM210 -DXPLM200 -DAPL -DGL_SILENCE_DEPRECATION -ISDK/CHeaders/XPLM XTextureExtractor.cpp XTextureExtractorNetwork.cpp lodepng/lodepng.cpp -shared -rdynamic -nodefaultlibs -framework OpenGL -FSDK/Libraries/Mac -framework XPLM -framework XPWidgets -o Plugin-XTextureExtractor-x64-Release/64/mac.xpl
+clang++ -arch x86_64 \
+  -flat_namespace -undefined suppress \
+  -std=c++17 -fPIC -Wno-deprecated-declarations \
+  -DXPLM301 -DXPLM300 -DXPLM210 -DXPLM200 -DAPL -DGL_SILENCE_DEPRECATION \
+  -ISDK/CHeaders/XPLM XTextureExtractor.cpp XTextureExtractorNetwork.cpp lodepng/lodepng.cpp \
+  -shared -rdynamic -nodefaultlibs -framework OpenGL -FSDK/Libraries/Mac -framework XPLM -framework XPWidgets \
+  -o Plugin-XTextureExtractor-x64-Release/64/mac.xpl
