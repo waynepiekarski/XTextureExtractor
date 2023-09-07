7892f5d4639b546b0ffa1907ccc4dcb063b0453d
Wayne Piekarski <wayne@tinmith.net>
2023-09-07 10:40:45 -0700

    Build a combined OSX plugin now with arm64 support as well

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -3,10 +3,12 @@
 # https://developer.x-plane.com/article/building-and-installing-plugins/
 cd `dirname $0`/..
 set -x
-clang++ -arch x86_64 \
+clang++ -arch x86_64 -arch arm64 \
   -std=c++17 -fPIC -Wno-deprecated-declarations \
   -DXPLM301 -DXPLM300 -DXPLM210 -DXPLM200 -DAPL -DGL_SILENCE_DEPRECATION \
   -ISDK/CHeaders/XPLM XTextureExtractor.cpp XTextureExtractorNetwork.cpp lodepng/lodepng.cpp \
   -shared -rdynamic \
   -framework OpenGL -FSDK/Libraries/Mac -framework XPLM -framework XPWidgets \
   -o Plugin-XTextureExtractor-x64-Release/64/mac.xpl
+
+file Plugin-XTextureExtractor-x64-Release/64/mac.xpl
