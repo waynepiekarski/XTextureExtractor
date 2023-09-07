f71c9d62ea74bb076b5f1f66a9f4a7db3b175c29
Wayne Piekarski <wayne@tinmith.net>
2023-09-07 10:36:59 -0700

    Remove unnecessary build flags and now compiles for x86 again with XPSDK401

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -4,9 +4,9 @@
 cd `dirname $0`/..
 set -x
 clang++ -arch x86_64 \
-  -flat_namespace -undefined suppress \
   -std=c++17 -fPIC -Wno-deprecated-declarations \
   -DXPLM301 -DXPLM300 -DXPLM210 -DXPLM200 -DAPL -DGL_SILENCE_DEPRECATION \
   -ISDK/CHeaders/XPLM XTextureExtractor.cpp XTextureExtractorNetwork.cpp lodepng/lodepng.cpp \
-  -shared -rdynamic -nodefaultlibs -framework OpenGL -FSDK/Libraries/Mac -framework XPLM -framework XPWidgets \
+  -shared -rdynamic \
+  -framework OpenGL -FSDK/Libraries/Mac -framework XPLM -framework XPWidgets \
   -o Plugin-XTextureExtractor-x64-Release/64/mac.xpl
